322efdb0078ef12c67ab4f6b556f1dc2341ba592
arkan <arkanshaheen30@gmail.com>
2023-04-08 21:37:00 -0400

    Adjusting the app.js query method to take in a query as a parameter

--- app.js
+++ app.js
@@ -1,31 +1,43 @@
 const { Client } = require("pg");
 const client = new Client(process.env.DATABASE_URL);
-const port = 8083;
+const port = 8084;
 var express = require('express');
+var bodyParser = require('body-parser')
 var app = express()
 let courses = [];
 
-(async () => {
-  await client.connect();
-  try {
-    courses = await client.query("SELECT courseId FROM courses");
-    console.log(courses);
-  } catch (err) {
-    console.error("error executing query:", err);
-  } finally {
-    client.end();
-  }
-})();
+async function perform_query(query) {
+    await client.connect();
+    try {
+        courses = await client.query(query);
+        //console.log(courses);
+    } catch (err) {
+        console.error("error executing query:", err);
+    } finally {
+        client.end();
+    }
+};
+
+perform_query("SELECT courseid FROM courses");
+
 
 app.use(express.static(__dirname));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
-app.get('/courseList', function(req, res) {
+app.get('/', function (req, res) {
     console.log('i receive a GET request');
 
     res.json(courses)
-  });
+});
 
-var server = app.listen(8083, function () {
+app.post('/analyzer', (req, res) => {
+    const result = req.body.myCourse;
+    console.log(result);
+    res.send("Data received" + result);
+});
+
+var server = app.listen(port, function () {
 
     var host = server.address().address
     var port = server.address().port
