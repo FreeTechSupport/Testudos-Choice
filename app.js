874e27ba0bbfbb99bb1c0477d76c9a5cfb71e579
Backedman <backedman@gmail.com>
2023-04-09 06:02:03 -0400

    finished chatgpt implementation

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const port = 8084;
 var express = require('express');
 var bodyParser = require('body-parser')
 var app = express()
+let result = ""
+
 
 async function query(query) {
     const client = new Client(process.env.DATABASE_URL);
@@ -30,15 +32,19 @@ app.get('/', function (req, res) {
 });
 
 app.post('/analyzer', (req, res) => {
-    const result = req.body.myCourse; 
-    res.redirect('/results?value=' + result);
+    result = req.body.myCourse; 
+    res.sendFile(__dirname + "/test.html")
 });
 
 app.get('/results', async (req, res) => {
-    const value = req.query.value;
-    console.log(value);
+    //console.log(value);
 
     let data = null;
+    let courses = await query("SELECT * FROM courses WHERE courseid = " + "'" + result + "'");
+    let profs = await query("SELECT * FROM Professors WHERE courseid = " + "'" + result + "'");
+    let response = await processCourseAndProfs(courses, profs)
+    res.json(response);
+    /*
     await query("SELECT * FROM courses WHERE courseid = " + "'" + value + "'").then(courses => 
         query("SELECT * FROM Professors WHERE courseid = " + "'" + value + "'").then(profs =>
 
@@ -59,12 +65,12 @@ app.get('/results', async (req, res) => {
 
 
 
-    //res.sendFile(__dirname + '/test.html');
+    //res.sendFile(__dirname + '/test.html');*/
   });
 
 async function processCourseAndProfs(courses, profs) {
 
-    console.log(courses.rows);
+    //console.log(courses.rows);
     //console.log(profs.rows);
 
     //Store name of class
@@ -74,7 +80,7 @@ async function processCourseAndProfs(courses, profs) {
     course_median_gpa = courses.rows[0].median_gpa
     course_professors = courses.rows[0].professors.split(',')
 
-    console.log(profs.rows[0])
+    //console.log(profs.rows[0])
     
 
     //get descriptions of professors and classes and comparisons by making a series of chatgpt queries
@@ -82,8 +88,67 @@ async function processCourseAndProfs(courses, profs) {
     let prompt = "";
     let super_prompt = "";
     let counter = 0;
+    let flag = false
     console.log("Profs: " + course_professors)
-    await course_professors.forEach(prof => {
+    //console.log(profs.rows)
+    for(const prof of course_professors) {
+        
+        for(const elem of profs.rows) {
+            if(prof == elem.professor && courseid == elem.courseid){
+                flag = true
+                counter++;
+                if(reviews.length < 2000) {
+                  reviews += counter + ". " + elem.review + " \n"
+                }
+                else{
+
+                  let res = await GPT35Turbo("Based on the course description: " + course_description + " and using the reviews listed below Write 10 key phrases in list form related to professor " + prof + " and how they conducts the class. If there are no reviews, do not give key phrases and simply note the lack of information about the professor " + "\n " + reviews);
+                  prompt += res;
+                  reviews = "";
+                  //
+                  /*(GPT35Turbo("Write 10 key phrases in list form related to professor " + prof + " and how he conducts the class").then(response => {prompt += response}));
+                  console.log("The prompt: " + prompt)
+                  reviews = "";
+                  counter = 0;
+                  */
+                }
+              }
+
+            
+              
+
+        }
+
+        if(flag == false){
+            super_prompt += "There is no reviews for professor " + prof + " for this class and thus it is hard to determine the professor's teaching quality."
+           
+        } else {
+
+            let res = await GPT35Turbo("Based on the course description: " + course_description + " and using the reviews listed below Write 10 key phrases in list form related to professor " + prof);
+            prompt += res;
+
+            super_prompt += await GPT35Turbo("using the key phrases about professor " + prof + ", give me a 100 word description on the professor's skill or lack thereof in teaching the class. If there is a lack of reviews for a professor, take that into consideration. \n\n" + prompt) + "\n\n";
+            //console.log(super_prompt);
+        }
+
+
+
+
+
+
+    }
+
+    course_analysis_prompt = "course description: " + course_description + "\n\n median gpa of the class: " + course_median_gpa + "\n\n Description of Professors: " + super_prompt + "\n\n give me an analysis of the course and its professors, along with recommendations on which professor should be targeted and which should be avoided. Give me an objective response."
+
+    course_analysis = await GPT35Turbo(course_analysis_prompt);
+    console.log(course_analysis);
+
+    //console.log({courseid:courseid, coursename: coursename, course_description: course_description, course_median_gpa: course_median_gpa, professors: course_professors, course_analysis:course_analysis})
+
+    return {courseid:courseid, coursename: coursename, course_description: course_description, course_median_gpa: course_median_gpa, professors: course_professors, course_analysis:course_analysis}
+
+
+    /*course_professors.forEach(prof => {
 
         profs.rows.forEach(elem => {
 
@@ -93,28 +158,26 @@ async function processCourseAndProfs(courses, profs) {
                 reviews += counter + ". " + elem.review + " /n"
               }
               else{
-                let response = GPT35Turbo("Write 10 key phrases in list form related to professor " + prof + " and how he conducts the class").then(res => {console.log("hi"); return res});
-                prompt += response
-                console.log(prompt)
+                async () => (await GPT35Turbo("Using the reviews listed below Write 10 key phrases in list form related to professor " + prof + " and how he conducts the class " + "\n " + reviews)).then(res => prompt += res);
                 reviews = ""
                 //
                 /*(GPT35Turbo("Write 10 key phrases in list form related to professor " + prof + " and how he conducts the class").then(response => {prompt += response}));
                 console.log("The prompt: " + prompt)
                 reviews = "";
                 counter = 0;
-                */
+                
               }
             }
     
         })
 
         //use the key phrases to get a description
-        super_prompt += GPT35Turbo("using these key phrases about professor " + prof + ", give me a 100 word description on the professor's notable points and quality \n\n" + prompt).then(response => {return response});
-        console.log(super_prompt)
+        super_prompt += GPT35Turbo("using these key phrases about professor " + prof + ", give me a 100 word description on the professor's skill or lack thereof in teaching the class \n\n" + prompt);
+        console.log(super_prompt);
 
 
 
-    })
+    })*/
     
 
     //make chatgpt use the previous query result to determine the best professor
@@ -151,12 +214,6 @@ async function GPT35Turbo(message) {
   
     return (response.data.choices[0].message.content);
   };
-
-  async function gptResponse(prof) {
-    let response = await GPT35Turbo("Write 10 key phrases in list form related to professor " + prof + " and how he conducts the class");
-    console.log(response);
-    return response;
-  }
 
 
 
